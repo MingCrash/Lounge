e1450d1273377dd2f5c55406565d4c09b406898d
zhuzhiming <zhiming.a.zhu@accenture.com>
2017-03-01 15:39:35 +0800

    Initial Commit

---
+++ Inventory Of Lounge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inventory Of Lounge
+//
+//  Created by 朱志明 on 2017/3/1.
+//  Copyright © 2017年 朱志明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Inventory Of LoungeTests/Inventory_Of_LoungeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Inventory_Of_LoungeTests.swift
+//  Inventory Of LoungeTests
+//
+//  Created by 朱志明 on 2017/3/1.
+//  Copyright © 2017年 朱志明. All rights reserved.
+//
+
+import XCTest
+@testable import Inventory_Of_Lounge
+
+class Inventory_Of_LoungeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
