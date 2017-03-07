cc063d6aeb37d876e7198ac46c1759e016209c01
zhuzhiming <zhiming.a.zhu@accenture.com>
2017-03-07 14:12:19 +0800

    commit for update at Mar 7

--- Inventory Of Lounge/LoungeViewControllerTableViewController.swift
+++ Inventory Of Lounge/LoungeViewControllerTableViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class LoungeViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var lastSction: Int? = nil
-    var RoungeDictionary: NSMutableDictionary?
-    var ItemsArray: NSMutableArray?
+    
+    var lastSction: Int? = 0
+    var RoungeDictionary: NSMutableDictionary? = nil
+    //被展开的items的数据组，直接唯一地控制整个tableView显示的对应结构体
+    var currentItemsArray: NSMutableArray? = NSMutableArray()
     var roungeArray: NSArray? = nil
     
     override func viewWillAppear(_ animated: Bool) {
@@ -21,6 +23,12 @@ class LoungeViewController: UIViewController {
         let dataPath = Bundle.main.path(forResource: "data", ofType: "plist")
         RoungeDictionary = NSMutableDictionary(contentsOfFile: dataPath!)
         roungeArray = RoungeDictionary?.allKeys as NSArray?
+        
+        //获取一个具有结构的而且空白的结构
+        for _ in RoungeDictionary! {
+            let element: NSMutableArray = NSMutableArray()
+            currentItemsArray?.add(element)
+        }
     }
     
     override func viewDidLoad() {
@@ -41,7 +49,6 @@ class LoungeViewController: UIViewController {
         navigationItem.rightBarButtonItem = add
     }
     
-    
     @objc func addTabbed() {
         
     }
@@ -58,7 +65,7 @@ extension LoungeViewController: UITableViewDelegate,UITableViewDataSource {
         let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "LoungeCellViewID") as! LoungeHeaderView
         headerView.delegate = self
         headerView.section = section
-        headerView.LoungeLabel.text = RoungeDictionary?.allKeys[section] as! String?
+        headerView.LoungeLabel.text = roungeArray?.object(at: section) as? String
         return headerView
     }
     
@@ -67,44 +74,59 @@ extension LoungeViewController: UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50.0
+        return 40.0
     }
-    
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 0.1
     }
     
+    //控制cell的行数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let name = roungeArray?.object(at: section) as! String
-        return (RoungeDictionary!.object(forKey: name) as! NSArray).count
+        return (currentItemsArray?.object(at: section) as! NSMutableArray).count
     }
     
+    //给cell安装cellView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "ValueTableViewCellViewID", for: indexPath) as! ValueTableViewCell
         return cell
     }
     
     func numberOfSections(in: UITableView) -> Int{
-        return (RoungeDictionary?.count)!
+        return (roungeArray?.count)!
     }
 }
 
 extension LoungeViewController: LoungeHeaderViewDelegate{
     func tapHeaderViewWith(section: Int){
-        if lastSction != nil {
+        //拿到休息室名
+        let name = roungeArray?.object(at: section) as! String
+        //休息室所对应的items
+        let tmpItemsArray = RoungeDictionary!.object(forKey: name) as! NSMutableArray?
+        
+        if section != lastSction {
+            currentItemsArray?.replaceObject(at: section, with: tmpItemsArray as Any)
+            currentItemsArray?.replaceObject(at: lastSction!, with: NSMutableArray())
             
-            if section != lastSction {
-                let roungeName = roungeArray?.object(at: section)
-                let sourceArray = RoungeDictionary?.object(forKey: roungeName)
-                
-                
+            var indexSet = IndexSet()
+            indexSet.insert(section)
+            indexSet.insert(lastSction!)
+            tableView.reloadSections(indexSet, with: .automatic)
+  
+            lastSction = section
+        }else{
+            //当前点击的section与之前的一致时候，
+            let countOfItems = (currentItemsArray?.object(at: section) as! NSMutableArray).count
+            
+            if countOfItems != 0 {
+                currentItemsArray?.replaceObject(at: section, with: NSMutableArray())
+            }else{
+                currentItemsArray?.replaceObject(at: section, with: tmpItemsArray as Any)
             }
             
-        }else{
-        
+            tableView.reloadSections(IndexSet(integer: section), with: .automatic)
         }
-
+  
     }
     
     func deleteHeaderViewWith(section :Int){
